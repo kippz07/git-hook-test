8c712f4404b7054b2ae42c18bff493bca6991540
jenny.johnson <jenny_johnson2@comcast.com>
2018-04-06 10:41:29 +0100

    hi

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+# webhook_url = "https://hooks.slack.com/services/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"
+webhook_url = "https://hooks.slack.com/services/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"
+slack_data = {"text": "woop woop!"}
+
+response = requests.post(
+    webhook_url, data=json.dumps(slack_data),
+    headers={"Content-Type": "application/json"}
+)
+if response.status_code != 200:
+    raise ValueError(
+        "Request to slack returned an erro %s, the response is:\n%s"
+        % (response.status_code, response.test)
+    )
