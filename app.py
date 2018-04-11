0df11c6ea77ea692be263ebfea059a2f4b9905a6
jenny.johnson <jenny_johnson2@comcast.com>
2018-04-11 14:17:18 +0100

    added another change

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import json
 import requests
 
-# webhook_url = "https://hooks.slack.com/services/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"
-webhook_url = "https://hooks.slack.com/services/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"
-slack_data = {"text": "woop woop!"}
+webhook_url = "https://hooks.slack.com/services/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"
+# webhook_url = "https://hooks.slack.com/services/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"
+slack_data = {"text": "magic"}
 
 response = requests.post(
     webhook_url, data=json.dumps(slack_data),
@@ -15,18 +15,5 @@ if response.status_code != 200:
         % (response.status_code, response.test)
     )
 # adding a comment
-# adding a comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
-# adding another comment
 # adding another comment
 # adding another comment
